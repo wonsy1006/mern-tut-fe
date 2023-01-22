5b917389ec36021731b8c435d8e4dee8008e807a
wonsy1006 <wonsy1006@gmail.com>
2023-01-23 01:24:49 +0900

    chapter 6: Redux & RTK Query

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -1,5 +1,52 @@
+import { useGetUsersQuery } from './usersApiSlice';
+import User from './User';
+
 const UsersList = () => {
-  return <h1>UsersList</h1>;
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUsersQuery();
+
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message}</p>;
+  }
+
+  if (isSuccess) {
+    const { ids } = users;
+    const tableContent = ids?.length
+      ? ids.map((userId) => <User key={userId} userId={userId} />)
+      : null;
+
+    content = (
+      <table className="table table--users">
+        <thead className="table__thead">
+          <tr>
+            <th className="table__th user__username" scope="col">
+              Username
+            </th>
+            <th className="table__th user__roles" scope="col">
+              Roles
+            </th>
+            <th className="table__th user__edit" scope="col">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>{tableContent}</tbody>
+      </table>
+    );
+
+    return content;
+  }
 };
 
 export default UsersList;
