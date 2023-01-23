82ffb1e6446bc589e5144c7bfaed82a4afa67b8a
wonsy1006 <wonsy1006@gmail.com>
2023-01-23 15:21:26 +0900

    working on NewNoteForm, EditNoteForm

--- src/features/notes/EditNoteForm.js
+++ src/features/notes/EditNoteForm.js
@@ -1,5 +1,39 @@
-const EditNoteForm = () => {
-  return <div>EditNoteForm</div>;
+import { useState, useEffect } from 'react';
+import { useUpdateNoteMutation, useDeleteNoteMutation } from './notesApiSlice';
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSave, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+
+const EditNoteForm = ({ users, note }) => {
+  const [updataNote, { isLoading, isSuccess, isError, error }] =
+    useUpdateNoteMutation();
+
+  const [
+    deleteNote,
+    { isSuccess: isDelSuccess, isError: isDelError, error: delError },
+  ] = useDeleteNoteMutation();
+
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState(note.title);
+  const [text, setText] = useState(note.text);
+  const [completed, setCompleted] = useState(note.completed);
+  const [userId, setUserId] = useState(note.user);
+
+  useEffect(() => {
+    console.log(isSuccess);
+    if (isSuccess || isDelSuccess) {
+      setTitle('');
+      setText('');
+      setUserId('');
+      navigate('/dash/notes');
+    }
+  }, [isSuccess, isDelSuccess, navigate]);
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onTextChanged = (e) => setText(e.target.value);
+  const onCompleteChanged = (e) => setCompleted((prev) => !prev);
+  const onUserIdChanged = (e) => setUserId(e.target.value);
 };
 
 export default EditNoteForm;
